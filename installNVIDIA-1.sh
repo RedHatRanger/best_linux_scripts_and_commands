6c158e927cd0fe16cd21bff93f2ce4027ad6c6ee
Brian MacFarlane <90477448+RedHatRanger@users.noreply.github.com>
2024-05-15 09:16:54 -0500

    Update installNVIDIA-1.sh

--- installNVIDIA-1.sh
+++ installNVIDIA-1.sh
@@ -16,7 +16,7 @@ if [[ $? -eq 1 ]]; then
   exit
 fi
 
-# See which NVIDIA card is present
+# See which NVIDIA card is present and copy the necessary NVIDIA driver file to /root
 gcard=$(lspci | grep -i nvidia | awk -F: 'NR==1{print $0}')
 if [[ $gcard =~ "P2000" ]] || [[ $gcard =~ "P6000" ]] || [[ $gcard =~ "K2200" ]]; then
   clear
@@ -27,4 +27,27 @@ elif [[ $gcard =~ "2235" ]]; then
   echo -e "\nCopying the necessary NVIDIA INstaller file to /root...Please wait...\n"
   rsync -av --progress /<some network share path>/NVIDIA-Linux-x86_64-${driver2}.run /root &>/dev/null
 else
+  clear
+  echo -e "\nCopying the necessary NVIDIA INstaller file to /root...Please wait...\n"
+  rsync -av --progress /<some network share path>/NVIDIA-Linux-x86_64-${driver3}.run /root &>/dev/null
+fi
+
+# Copy the information about blacklisting the Nouveau driver that comes packaged with RHEL
+grep "blacklist nouveau" /etc/modprobe.d/blacklist.conf &>/dev/null
+if [[ $? -eq 1 ]]; then
+cat << EOF >> /etc/modprobe.d/blacklist.conf
+blacklist nouveau
+options nouveau modeset=0
+EOF
+# Adjust the default grub loader file
+sed -i 's/quiet/quiet modprobe.blacklist=nouveau/g' /etc/default/grub
+# Regenerate the newly modified grub file
+grub2-mkconfig -o `find /boot -name "grub.cfg" | grep EFI`
+fi
+
+# Set the default target to multi-user.target to prepare for the new driver install upon reboot
+systemctl set-default multi-user.target
+
+# Reboot the system
+init 6
 ```
