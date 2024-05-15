1e0b6110e6254c852233f6bbcd48c47e2045139c
Brian MacFarlane <90477448+RedHatRanger@users.noreply.github.com>
2024-05-15 14:25:32 -0500

    Create gather-HDD-information.sh

---
+++ scripts/gather-HDD-information.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script will gather HDD serial #'s for multiple machines and store them on a network share.
+
+mkdir -p /<some network share>/HDD-Info/$(hostname -s)
+echo "lsblk output:" > /<some network share>/HDD-Info/$(hostname -s)/HDD_information.txt
+lsblk >> /<some network share>/HDD-Info/$(hostname -s)/HDD_information.txt
+echo " "
+echo " "
+for i in {a..z}; do echo "smartctl -i /dev/sd${i} output:"; smartctl -i /dev/sd${i}; done >> /<some network share>/HDD-Info/$(hostname -s)/HDD_information.txt
+echo "df -h output:" >> /<some network share>/HDD-Info/$(hostname -s)/HDD_information.txt
+df -h >> /<some network share>/HDD-Info/$(hostname -s)/HDD_information.txt
