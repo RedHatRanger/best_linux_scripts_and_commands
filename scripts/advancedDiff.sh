bc687d4a4a298e1ddb16a133204a366661368f7f
Brian MacFarlane <90477448+RedHatRanger@users.noreply.github.com>
2024-05-18 10:14:31 -0500

    Update advancedDiff.sh

--- scripts/advancedDiff.sh
+++ scripts/advancedDiff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This script compares the contents of two files and \
-# outputs the items from the reference file (file1) that \
+# This script compares the contents of two files and
+# outputs the items from the reference file (file1) that
 # are missing in the comparison file (file2)
 
 for i in $(cat /path/to/file1); do 
