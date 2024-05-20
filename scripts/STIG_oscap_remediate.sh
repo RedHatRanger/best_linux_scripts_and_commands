262a007b97a91b13c16e2545bd3559450b241252
Brian MacFarlane <90477448+RedHatRanger@users.noreply.github.com>
2024-05-20 10:18:00 -0500

    Update STIG_oscap_remediate.sh

--- scripts/STIG_oscap_remediate.sh
+++ scripts/STIG_oscap_remediate.sh
@@ -3,4 +3,4 @@
 # This script will evaluate a system against the DISA STIGs and generate an ansible playbook to remediate it.
 
 yum -y install openscap-scanner scap-security-guide
-oscap xccdf generate fix --profile xccdf_org.ssgproject.content_profile_stig --fetch-remote-resources --template urn:xccdf:fix:script:ansible /usr/share/xml/scap/ssg/content/ssg-rhel9-ds.xml > /tmp/remediation_playbook.yml
+oscap xccdf generate fix --profile stig --fetch-remote-resources --template urn:xccdf:fix:script:ansible /usr/share/xml/scap/ssg/content/ssg-rhel9-ds.xml > /tmp/remediation_playbook.yml
