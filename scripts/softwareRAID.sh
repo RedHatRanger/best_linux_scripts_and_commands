31f8961ff908d7f9e1e597c7ec57eb2e5d5160ff
RedHatRanger <90477448+RedHatRanger@users.noreply.github.com>
2025-02-19 10:14:27 -0600

    Update softwareRAID.sh

--- scripts/softwareRAID.sh
+++ scripts/softwareRAID.sh
@@ -1,3 +1,4 @@
+```
 #!/bin/bash
 
 # This script will create a software raid using 12 other mounted hard drives. In this example the OS resides on /dev/sda.
@@ -8,5 +9,5 @@ mkfs.ext4 /dev/md0
 mkdir /mnt/raid
 echo "/dev/md0 /mnt/raid           ext4        defaults 0 0" >> /etc/fstab
 
-
 # To check: mdadm --details /dev/md0
+```
