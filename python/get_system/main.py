226e65d78f7abd89569638d77a923c7c34301a09
RedHatRanger <90477448+RedHatRanger@users.noreply.github.com>
2025-04-16 10:23:37 -0500

    Update main.py

--- python/get_system/main.py
+++ python/get_system/main.py
@@ -4,8 +4,11 @@
 # uv python install 3.9.21
 # uv run --python 3.8 main.py
 # uv run --with rich --with requests --python 3.8 main.py
+
+### But if you don't want to type this out every time:
 # uv init --script main.py --python 3.9.21
 # uv add --script main.py "rich"
+# uv add --script main.py "requests"
 
 import sys
 from rich import print
