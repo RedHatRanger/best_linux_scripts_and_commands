d863364da69f83c79f1ad596d20332b3f5603cfb
RedHatRanger <90477448+RedHatRanger@users.noreply.github.com>
2025-04-16 10:20:58 -0500

    Update main.py

--- python/get_system/main.py
+++ python/get_system/main.py
@@ -1,8 +1,10 @@
 ```
 # pip3 install uv
 # uv python install 3.8
+# uv python install 3.9.21
 # uv run --python 3.8 main.py
 # uv run --with rich --with requests --python 3.8 main.py
+# uv init --script main.py --python 3.9.21
 
 import sys
 from rich import print
